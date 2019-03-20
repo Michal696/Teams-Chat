745f85edff3f2333654c5cea7c7e426003e17f35
Michal696 <30591067+Michal696@users.noreply.github.com>
2019-03-20 17:57:29 +0100

    description

--- Teams/Models/Entities/Media.cs
+++ Teams/Models/Entities/Media.cs
@@ -4,8 +4,9 @@ namespace Teams.Models.Entities
 {
     public class Media
     {
-        public String Url { get; set; }
-        public Message Parent { get; set; }
+        //public String Url { get; set; }
+        //public Message Parent { get; set; }
+        // waiting for more  members to confirm, so we can delete this file :D
 
     }
 }
