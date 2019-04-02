90b971d8f4ab01c67376cf87777b1054e4a0378f
Filip Pleško <xplesk02@vutbr.cz>
2019-04-02 18:52:02 +0200

    testujeme ci to facha

--- Teams/Models/Entities/Media.cs
+++ Teams/Models/Entities/Media.cs
@@ -6,7 +6,7 @@ namespace Teams.Models.Entities
     {
         //public String Url { get; set; }
         //public Message Parent { get; set; }
-        // waiting for more  members to confirm, so we can delete this file :D
+        // waiting for more  members to confirm, so we can delete this file
 
     }
 }
