a907031619c026c549bbed37c2ff9d2cd0ebf4f3
Filip Pleško <xplesk02@vutbr.cz>
2019-04-03 22:38:56 +0200

    Database initial migrate

---
+++ Teams.DAL/Migrations/20190403195457_InitialCreate.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Teams.DAL.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Team",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Team", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    TeamId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Groups_Team_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Team",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    LastLogin = table.Column<DateTime>(nullable: false),
+                    TeamId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Team_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Team",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupsUserPermissions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    GroupId = table.Column<Guid>(nullable: true),
+                    MemberId = table.Column<Guid>(nullable: true),
+                    Permit = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupsUserPermissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GroupsUserPermissions_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_GroupsUserPermissions_Users_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Media",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Media", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Media_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    TimeStamp = table.Column<DateTime>(nullable: false),
+                    Text = table.Column<string>(nullable: true),
+                    MemberId = table.Column<Guid>(nullable: true),
+                    ParentId = table.Column<Guid>(nullable: true),
+                    GroupId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Messages_Messages_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "Messages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    TimeStamp = table.Column<DateTime>(nullable: false),
+                    Text = table.Column<string>(nullable: true),
+                    MemberId = table.Column<Guid>(nullable: true),
+                    State = table.Column<int>(nullable: false),
+                    GroupId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Users_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TeamMember",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    MemberId = table.Column<Guid>(nullable: true),
+                    TeamId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamMember", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TeamMember_Users_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TeamMember_Team_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Team",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskStateChange",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    TimeStamp = table.Column<DateTime>(nullable: false),
+                    Text = table.Column<string>(nullable: true),
+                    MemberId = table.Column<Guid>(nullable: true),
+                    State = table.Column<int>(nullable: false),
+                    GroupId = table.Column<Guid>(nullable: true),
+                    TaskId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskStateChange", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TaskStateChange_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TaskStateChange_Users_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TaskStateChange_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TaskAssignments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    MemberId = table.Column<Guid>(nullable: true),
+                    TaskId = table.Column<Guid>(nullable: true),
+                    TaskStateChangeId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskAssignments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TaskAssignments_Users_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TaskAssignments_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TaskAssignments_TaskStateChange_TaskStateChangeId",
+                        column: x => x.TaskStateChangeId,
+                        principalTable: "TaskStateChange",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_TeamId",
+                table: "Groups",
+                column: "TeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupsUserPermissions_GroupId",
+                table: "GroupsUserPermissions",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupsUserPermissions_MemberId",
+                table: "GroupsUserPermissions",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Media_UserId",
+                table: "Media",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_GroupId",
+                table: "Messages",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_MemberId",
+                table: "Messages",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ParentId",
+                table: "Messages",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskAssignments_MemberId",
+                table: "TaskAssignments",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskAssignments_TaskId",
+                table: "TaskAssignments",
+                column: "TaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskAssignments_TaskStateChangeId",
+                table: "TaskAssignments",
+                column: "TaskStateChangeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_GroupId",
+                table: "Tasks",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_MemberId",
+                table: "Tasks",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskStateChange_GroupId",
+                table: "TaskStateChange",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskStateChange_MemberId",
+                table: "TaskStateChange",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskStateChange_TaskId",
+                table: "TaskStateChange",
+                column: "TaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeamMember_MemberId",
+                table: "TeamMember",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeamMember_TeamId",
+                table: "TeamMember",
+                column: "TeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_TeamId",
+                table: "Users",
+                column: "TeamId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GroupsUserPermissions");
+
+            migrationBuilder.DropTable(
+                name: "Media");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "TaskAssignments");
+
+            migrationBuilder.DropTable(
+                name: "TeamMember");
+
+            migrationBuilder.DropTable(
+                name: "TaskStateChange");
+
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Team");
+        }
+    }
+}
