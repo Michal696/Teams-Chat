461bce8b09458f9aec86f2e7eb587729159b2c51
Tomáš Lapšanský <xlapsa00@vutbr.cz>
2019-04-05 14:09:39 +0200

    BL basic

---
+++ ClassLibrary1/Models/GroupUserPermissionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Teams.BL.Models;
+using Teams.BL.Models.Base;
+
+namespace Teams.BL.Models
+{
+    public class GroupUserPermissionModel : ModelBase
+    {
+        public GroupModel Group { get; set; }
+
+        public UserModel Member { get; set; }
+        public Permission Permit { get; set; }
+    }
+}
---
+++ ClassLibrary1/Models/TeamModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Teams.BL.Models.Base;
+
+namespace Teams.BL.Models
+{
+    public class TeamModel : ModelBase
+    {
+        public String Name { get; set; }
+        public virtual ICollection<UserModel> Members { get; set; }
+    }
+}
