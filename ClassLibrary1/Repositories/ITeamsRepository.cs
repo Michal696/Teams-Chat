79470e5ba046061c0f2e4f28a85abb653af8109e
Tomáš Lapšanský <xlapsa00@vutbr.cz>
2019-04-07 19:56:29 +0200

    IRepository add

---
+++ ClassLibrary1/Repositories/ITeamsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teams.BL.Models;
+
+namespace Teams.BL.Repositories
+{
+    interface ITeamsRepository
+    {
+        IEnumerable<TeamModel> GetAll();
+        TeamModel GetById(Guid Id);
+        IEnumerable<TeamModel> GetByUser(Guid Id);
+        void Create(TeamModel Team);
+        void Delete(Guid Id);
+        void Update(TeamModel Team);
+        void AddUserToGroup(Guid GroupId, Guid UserId); /* TODO add permision */
+    }
+}
