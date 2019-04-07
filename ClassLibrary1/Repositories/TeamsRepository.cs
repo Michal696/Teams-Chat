79d6962bfca834c5851997e02f9388939e0d722a
Michal696 <30591067+Michal696@users.noreply.github.com>
2019-04-07 20:42:24 +0200

    TeamsRepository

---
+++ ClassLibrary1/Repositories/TeamsRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teams.BL.Factories;
+using Teams.BL.Models;
+
+namespace Teams.BL.Repositories
+{
+    public class TeamsRepository : ITeamsRepository
+    {
+        private readonly IDbContextFactory dbContextFactory;
+
+        public TeamsRepository(IDbContextFactory dbContextFactory)
+        {
+            this.dbContextFactory = dbContextFactory;
+        }
+
+
+        void ITeamsRepository.AddUserToGroup(Guid GroupId, Guid UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ITeamsRepository.Create(TeamModel Team)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ITeamsRepository.Delete(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<TeamModel> ITeamsRepository.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        TeamModel ITeamsRepository.GetById(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<TeamModel> ITeamsRepository.GetByUser(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ITeamsRepository.Update(TeamModel Team)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
