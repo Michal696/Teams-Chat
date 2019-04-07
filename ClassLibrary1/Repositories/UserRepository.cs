f4a8f89628bc820d12d13126bad3aa73064abd81
Filip Pleško <xplesk02@vutbr.cz>
2019-04-07 18:06:15 +0200

    nknj

---
+++ ClassLibrary1/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teams.BL.Factories;
+
+namespace Teams.BL.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IDbContextFactory dbContextFactory;
+        public UserRepository(IDbContextFactory dbContextFactory)
+        {
+            this.dbContextFactory = dbContextFactory;
+        }
+    }
+}
