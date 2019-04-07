2a8e7ea0c63d9c284d967dcf18e33f89db4e1e95
Michal696 <30591067+Michal696@users.noreply.github.com>
2019-04-07 12:38:46 +0200

    New test for DAL, Renaming test project for DAL

--- Tests/EntityDbContextTests.cs
+++ Tests/EntityDbContextTests.cs
@@ -16,20 +16,6 @@ namespace Tests
             this.output = output;
         }
 
-        // TODO DELETE WHEN YOU SEE THIS and it is after 6.4.2019
-        //[Fact]
-        //public void SuccessfulTest()
-        //{
-        //    output.WriteLine("This should work");
-        //    Assert.Equal(10, 5 + 5);
-        //}
-
-        //[Fact]
-        //public void UnSuccessfulTest()
-        //{
-        //    Assert.Equal(10, 4 + 5);
-        //}
-
         [Fact]
         public void AddMessageTest()
         {
@@ -50,7 +36,40 @@ namespace Tests
             {
                 var retrievedMessage = dbx.Messages.First(entity => entity.Id == messageEntity.Id);
 
-                Assert.Equal(messageEntity, retrievedMessage, Message.DescriptionNameIdComparer);                
+                Assert.Equal(messageEntity, retrievedMessage, Message.DescriptionNameIdComparer);                  
+            }
+        }
+
+        [Fact]
+        public void AddMessageWithUser()
+        {
+            //Arrange
+            var messageEntity = new Message
+            {
+                Title = "testTitle",
+                Member = new User()
+                {
+                    Name = "testTitleForUser",
+                    Password = "NotEncryptedPassword"
+                }
+
+
+                
+            };
+
+            //Act
+            _testContext.EntityDbContextSUT.Messages.Add(messageEntity);
+            _testContext.EntityDbContextSUT.SaveChanges();
+
+
+            //Assert
+            using (var dbx = _testContext.CreateEntityDbContext())
+            {
+                var retrievedMessage = dbx.Messages
+                                    .First(entity => entity.Id == messageEntity.Id);
+                
+
+                Assert.Equal(messageEntity, retrievedMessage, Message.DescriptionNameIdComparer);
             }
         }
 
