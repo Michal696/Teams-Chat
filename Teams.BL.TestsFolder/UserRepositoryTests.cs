a6bfb8472f402b5b92fa13d62b3d450276af50a9
Michal696 <30591067+Michal696@users.noreply.github.com>
2019-04-07 21:07:59 +0200

    UserRepositoryTests

---
+++ Teams.BL.TestsFolder/UserRepositoryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teams.BL.Models;
+using Teams.BL.Tests;
+using Xunit;
+
+namespace Teams.BL.Tests
+{
+    public class UserRepositoryTests : IClassFixture<UserRepositoryTestsFixture>
+    {
+        private readonly UserRepositoryTestsFixture fixture;
+
+        public UserRepositoryTests(UserRepositoryTestsFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void Create_WithNonExistingItem_DoesNotThrow()
+        {
+            var model = new UserModel
+            {
+                Name = "TestUser - some random data"
+            };
+
+            var returnedModel = fixture.Repository.Create(model);
+            
+            Assert.NotNull(returnedModel);
+
+            fixture.Repository.Delete(returnedModel.Id);
+        }
+    }
+}
