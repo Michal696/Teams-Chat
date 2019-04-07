f35b1cb35ac937db63415044721aabbfc57cbd8c
Michal696 <30591067+Michal696@users.noreply.github.com>
2019-04-07 22:57:30 +0200

    TeamsRepositoryTestsFixture

---
+++ Teams.BL.TestsFolder/TeamsRepositoryTestsFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teams.BL.Repositories;
+
+namespace Teams.BL.Tests
+
+{
+    public class TeamsRepositoryTestsFixture
+    {
+        private readonly ITeamsRepository repository;
+        public TeamsRepositoryTestsFixture()
+        {
+            repository = new TeamsRepository(new InMemoryDbContextFactory());
+        }
+
+        public ITeamsRepository Repository => repository;
+    }
+}
