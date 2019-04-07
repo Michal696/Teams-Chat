4a119aed1a64cf38c875996d0aa219a52b655d07
Michal696 <30591067+Michal696@users.noreply.github.com>
2019-04-07 12:54:55 +0200

    added project for testing BL

---
+++ Teams.BL.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Teams.BL.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
