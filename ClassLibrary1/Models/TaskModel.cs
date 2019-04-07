6cecc1fef01026679db2eed19e0dcfc544c256d0
Michal696 <30591067+Michal696@users.noreply.github.com>
2019-04-07 17:14:18 +0200

    enum separation

--- ClassLibrary1/Models/TaskModel.cs
+++ ClassLibrary1/Models/TaskModel.cs
@@ -17,14 +17,4 @@ namespace Teams.BL.Models
 
 
     }
-
-    public enum TaskState
-    {
-        NEW = 0,
-        WORKING_ON = 1,
-        TO_BE_TESTED = 2,
-        TESTED = 3,
-        DEPLOYED = 4,
-    
-    }
 }
