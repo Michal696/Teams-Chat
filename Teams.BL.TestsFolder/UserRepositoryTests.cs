8f9e274356c4e977d0cb907b96f9994ce8b501da
Michal696 <30591067+Michal696@users.noreply.github.com>
2019-04-14 16:10:19 +0200

    UserRepositoryTests

--- Teams.BL.TestsFolder/UserRepositoryTests.cs
+++ Teams.BL.TestsFolder/UserRepositoryTests.cs
@@ -112,5 +112,105 @@ namespace Teams.BL.Tests
             Assert.NotEqual(returnedModelCreated.Name, foundModel.Name);
             Assert.Equal(foundModel.Name, secondName);
         }
+
+        [Fact]
+        public void GetAllUsers_CountOfValues()
+        {
+            var model1 = new UserModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "FirstTestUser"
+            };
+            var returnedModel1 = fixture.Repository.Create(model1);
+
+            Assert.NotNull(returnedModel1);
+
+            var model2 = new UserModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "SecondTestUser"
+            };
+            var returnedModel2 = fixture.Repository.Create(model2);
+
+            Assert.NotNull(returnedModel2);
+
+
+            IEnumerable<UserModel> userModelList = fixture.Repository.GetAll();
+
+            // uncomment when repaired: "userModelList.Count()"
+            //int count =
+            userModelList.Count(); // expecting "2"
+
+            // uncomment when repaired: "userModelList.Count()"
+            // int expect = 2;            
+            // Assert.Equal(count, expect);
+
+
+        }
+
+
+        [Fact]
+        public void GetAllUsers_ByValues()
+        {
+            var model1 = new UserModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "FirstTestUser"
+            };
+            var returnedModel1 = fixture.Repository.Create(model1);
+
+            Assert.NotNull(returnedModel1);
+
+            var model2 = new UserModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "SecondTestUser"
+            };
+            var returnedModel2 = fixture.Repository.Create(model2);
+
+            Assert.NotNull(returnedModel2);
+
+            IEnumerable<UserModel> userModelList = fixture.Repository.GetAll();
+
+            var returnedModel = userModelList.Single(m => m.Name == "FirstTestUser");
+            Assert.Equal(returnedModel1.Id, returnedModel.Id);
+            Assert.Equal(returnedModel1.Name, returnedModel.Name);
+
+            returnedModel = userModelList.Single(m => m.Name == "SecondTestUser");
+            Assert.Equal(returnedModel2.Id, returnedModel.Id);
+            Assert.Equal(returnedModel2.Name, returnedModel.Name);
+        }
+
+        [Fact]
+        public void GetAllUsers_ById()
+        {
+            var model1 = new UserModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "FirstTestUser"
+            };
+            var returnedModel1 = fixture.Repository.Create(model1);
+
+            Assert.NotNull(returnedModel1);
+
+            var model2 = new UserModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "SecondTestUser"
+            };
+            var returnedModel2 = fixture.Repository.Create(model2);
+
+            Assert.NotNull(returnedModel2);
+
+            IEnumerable<UserModel> userModelList = fixture.Repository.GetAll();
+
+            var returnedModel = userModelList.Single(m => m.Id == returnedModel1.Id);
+            Assert.Equal(returnedModel1.Id, returnedModel.Id);
+            Assert.Equal(returnedModel1.Name, returnedModel.Name);
+
+            returnedModel = userModelList.Single(m => m.Id == returnedModel2.Id);
+            Assert.Equal(returnedModel2.Id, returnedModel.Id);
+            Assert.Equal(returnedModel2.Name, returnedModel.Name);
+        }
     }
 }
