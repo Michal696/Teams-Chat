6f3e19e30b29bac77fa687f716a63b786974fcb2
Michal696 <30591067+Michal696@users.noreply.github.com>
2019-04-14 17:26:48 +0200

    TeamsRepositoryTests

--- Teams.BL.TestsFolder/TeamsRepositoryTests.cs
+++ Teams.BL.TestsFolder/TeamsRepositoryTests.cs
@@ -113,5 +113,331 @@ namespace Teams.BL.Tests
             Assert.NotEqual(returnedModelCreated.Name, foundModel.Name);
             Assert.Equal(foundModel.Name, secondName);
         }
+
+        [Fact]
+        public void AddUserToTeam_value()
+        {
+            var model = new TeamModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestTeamModel"
+            };
+            var returnedModel = fixture.Repository.Create(model);
+
+            Assert.NotNull(returnedModel);
+
+            UserModel userModel = new UserModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "UserName"
+            };
+
+            TeamModel teamModel = new TeamModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "TeamName"
+            };
+
+            var teamMemberModel = new TeamMemberModel()
+            {
+                Id = Guid.NewGuid(),
+                User = userModel,
+                Team = teamModel
+                    
+            };
+
+            TeamMemberModel returnedTeamMemberModel = fixture.Repository.AddUserToTeam(teamMemberModel);
+
+            Assert.Equal(teamMemberModel.Id, returnedTeamMemberModel.Id);
+
+            Assert.Equal(teamMemberModel.Team.Id, returnedTeamMemberModel.Team.Id);
+            Assert.Equal(teamMemberModel.Team.Name, returnedTeamMemberModel.Team.Name);
+
+
+            Assert.Equal(teamMemberModel.User.Id, returnedTeamMemberModel.User.Id);
+            Assert.Equal(teamMemberModel.User.Name, returnedTeamMemberModel.User.Name);
+        }
+
+
+        [Fact]
+        public void FindingTeamsByUserId_BasedOnUserId()
+        {
+            var teamModel1 = new TeamModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestTeamModel1"
+            };
+
+            var teamModel2 = new TeamModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestTeamModel2"
+            };
+
+            var teamModel3 = new TeamModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestTeamModel3"
+            };
+
+
+            var returnedModel1 = fixture.Repository.Create(teamModel1);
+            var returnedModel2 = fixture.Repository.Create(teamModel2);
+            var returnedModel3 = fixture.Repository.Create(teamModel3);
+
+            Assert.NotNull(returnedModel1);
+            Assert.NotNull(returnedModel2);
+            Assert.NotNull(returnedModel3);
+
+            UserModel userModel = new UserModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "UserName"
+            };
+
+
+            var teamMemberModel1 = new TeamMemberModel()
+            {
+                Id = Guid.NewGuid(),
+                User = userModel,
+                Team = teamModel1
+
+            };
+
+            TeamMemberModel returnedTeamMemberModel1 = fixture.Repository.AddUserToTeam(teamMemberModel1);
+
+            var teamMemberModel2 = new TeamMemberModel()
+            {
+                Id = Guid.NewGuid(),
+                User = userModel,
+                Team = teamModel2
+
+            };
+
+            TeamMemberModel returnedTeamMemberModel2 = fixture.Repository.AddUserToTeam(teamMemberModel2);
+
+
+            IEnumerable<TeamModel> teamModels = fixture.Repository.GetByUser(userModel.Id);
+
+            var returnedTeamModel1 = teamModels.Single(m => m.Id == returnedModel1.Id);
+            Assert.NotNull(returnedTeamModel1);
+            Assert.Equal(returnedTeamModel1.Id, teamModel1.Id);
+            Assert.Equal(returnedTeamModel1.Name, teamModel1.Name);
+
+            var returnedTeamModel2 = teamModels.Single(m => m.Id == returnedModel2.Id);
+            Assert.NotNull(returnedTeamModel2);
+            Assert.Equal(returnedTeamModel2.Id, teamModel2.Id);
+            Assert.Equal(returnedTeamModel2.Name, teamModel2.Name);
+
+
+            var returnedTeamModel3 = teamModels.Single(m => m.Id == returnedModel3.Id);
+            Assert.Null(returnedTeamModel1);
+
+
+        }
+
+
+        [Fact]
+        public void FindingTeamsByUserId_VerifyCount()
+        {
+            var teamModel1 = new TeamModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestTeamModel1"
+            };
+
+            var teamModel2 = new TeamModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestTeamModel2"
+            };
+
+            var teamModel3 = new TeamModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestTeamModel3"
+            };
+
+
+            var returnedModel1 = fixture.Repository.Create(teamModel1);
+            var returnedModel2 = fixture.Repository.Create(teamModel2);
+            var returnedModel3 = fixture.Repository.Create(teamModel3);
+
+            Assert.NotNull(returnedModel1);
+            Assert.NotNull(returnedModel2);
+            Assert.NotNull(returnedModel3);
+
+            UserModel userModel = new UserModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "UserName"
+            };
+
+
+            var teamMemberModel1 = new TeamMemberModel()
+            {
+                Id = Guid.NewGuid(),
+                User = userModel,
+                Team = teamModel1
+
+            };
+
+            TeamMemberModel returnedTeamMemberModel1 = fixture.Repository.AddUserToTeam(teamMemberModel1);
+
+            var teamMemberModel2 = new TeamMemberModel()
+            {
+                Id = Guid.NewGuid(),
+                User = userModel,
+                Team = teamModel2
+
+            };
+
+            TeamMemberModel returnedTeamMemberModel2 = fixture.Repository.AddUserToTeam(teamMemberModel2);
+
+            int countOfTeamModels = 2;
+            Assert.Equal(fixture.Repository.GetByUser(userModel.Id).Count(), countOfTeamModels);
+            
+        }
+
+        [Fact]
+        public void TeamsGetAll_values()
+        {
+            var teamModel1 = new TeamModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestTeamModel1"
+            };
+
+            var teamModel2 = new TeamModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestTeamModel2"
+            };
+
+            var teamModel3 = new TeamModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestTeamModel3"
+            };
+
+
+            var returnedModel1 = fixture.Repository.Create(teamModel1);
+            var returnedModel2 = fixture.Repository.Create(teamModel2);
+            var returnedModel3 = fixture.Repository.Create(teamModel3);
+
+            Assert.NotNull(returnedModel1);
+            Assert.NotNull(returnedModel2);
+            Assert.NotNull(returnedModel3);
+
+            UserModel userModel = new UserModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "UserName"
+            };
+
+
+            var teamMemberModel1 = new TeamMemberModel()
+            {
+                Id = Guid.NewGuid(),
+                User = userModel,
+                Team = teamModel1
+
+            };
+
+            TeamMemberModel returnedTeamMemberModel1 = fixture.Repository.AddUserToTeam(teamMemberModel1);
+
+            var teamMemberModel2 = new TeamMemberModel()
+            {
+                Id = Guid.NewGuid(),
+                User = userModel,
+                Team = teamModel2
+
+            };
+
+            TeamMemberModel returnedTeamMemberModel2 = fixture.Repository.AddUserToTeam(teamMemberModel2);
+
+
+            IEnumerable<TeamModel> teamModels = fixture.Repository.GetAll();
+
+            var returnedTeamModel1 = teamModels.Single(m => m.Id == returnedModel1.Id);
+            Assert.NotNull(returnedTeamModel1);
+            Assert.Equal(returnedTeamModel1.Id, teamModel1.Id);
+            Assert.Equal(returnedTeamModel1.Name, teamModel1.Name);
+
+            var returnedTeamModel2 = teamModels.Single(m => m.Id == returnedModel2.Id);
+            Assert.NotNull(returnedTeamModel2);
+            Assert.Equal(returnedTeamModel2.Id, teamModel2.Id);
+            Assert.Equal(returnedTeamModel2.Name, teamModel2.Name);
+
+
+            var returnedTeamModel3 = teamModels.Single(m => m.Id == returnedModel3.Id);
+            Assert.NotNull(returnedTeamModel2);
+            Assert.Equal(returnedTeamModel3.Id, teamModel3.Id);
+            Assert.Equal(returnedTeamModel3.Name, teamModel3.Name);
+
+
+        }
+
+        [Fact]
+        public void TeamsGetAll_countOfValues()
+        {
+            var teamModel1 = new TeamModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestTeamModel1"
+            };
+
+            var teamModel2 = new TeamModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestTeamModel2"
+            };
+
+            var teamModel3 = new TeamModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestTeamModel3"
+            };
+
+
+            var returnedModel1 = fixture.Repository.Create(teamModel1);
+            var returnedModel2 = fixture.Repository.Create(teamModel2);
+            var returnedModel3 = fixture.Repository.Create(teamModel3);
+
+            Assert.NotNull(returnedModel1);
+            Assert.NotNull(returnedModel2);
+            Assert.NotNull(returnedModel3);
+
+            UserModel userModel = new UserModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "UserName"
+            };
+
+
+            var teamMemberModel1 = new TeamMemberModel()
+            {
+                Id = Guid.NewGuid(),
+                User = userModel,
+                Team = teamModel1
+
+            };
+
+            TeamMemberModel returnedTeamMemberModel1 = fixture.Repository.AddUserToTeam(teamMemberModel1);
+
+            var teamMemberModel2 = new TeamMemberModel()
+            {
+                Id = Guid.NewGuid(),
+                User = userModel,
+                Team = teamModel2
+
+            };
+
+            TeamMemberModel returnedTeamMemberModel2 = fixture.Repository.AddUserToTeam(teamMemberModel2);
+
+            int countOfTeamModels = 3;
+            Assert.Equal(fixture.Repository.GetAll().Count(), countOfTeamModels);
+            
+        }
     }
 }
