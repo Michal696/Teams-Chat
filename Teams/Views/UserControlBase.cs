c5139fd0eced264b40cdc526fee83a606a28505f
Jakub Liška <xliska16@vutbr.cz>
2019-05-04 03:17:17 +0200

    user control base onload load

---
+++ Teams/Views/UserControlBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Teams.ViewModels;
+
+namespace Teams.Views
+{
+    public abstract class UserControlBase : UserControl
+    {
+        protected UserControlBase()
+        {
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is IViewModel viewModel)
+            {
+                viewModel.Load();
+            }
+        }
+    }
+}
