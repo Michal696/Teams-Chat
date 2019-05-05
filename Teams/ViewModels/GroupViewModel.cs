6a740794aae6d6875bf77373bf72b6882c059b57
Filip Pleško <xplesk02@vutbr.cz>
2019-05-05 12:08:18 +0200

    create delete group

--- Teams/ViewModels/GroupViewModel.cs
+++ Teams/ViewModels/GroupViewModel.cs
@@ -28,6 +28,7 @@ namespace Teams.ViewModels
         public TeamModel ModelTeam { get; set; }
 
         public ICommand AddNewGroupCommand { get; set; }
+        public ICommand DeleteGroupCommand { get; set; }
 
         public GroupViewModel(IGroupTaskRepository groupTaskRepository, ITeamsRepository teamsRepository, IMessageBoxService messageBoxService, IMediator mediator)
         {
@@ -37,10 +38,13 @@ namespace Teams.ViewModels
             this.mediator = mediator;
 
             AddNewGroupCommand = new RelayCommand(CreateNewGroup);
+            DeleteGroupCommand = new RelayCommand(DeleteGroup);
 
             mediator.Register<GroupNewMessage>(GroupNewAdded);
             mediator.Register<TeamSelectMessage>(TeamSelected);
+            mediator.Register<GroupDeleteMessage>(GroupDeleted);
         }
+
         private void CreateNewGroup()
         {
             Model = new GroupModel();
@@ -60,6 +64,28 @@ namespace Teams.ViewModels
         private void TeamSelected(TeamSelectMessage teamSelectMessage)
         {
             ModelTeam = teamsRepository.GetById(teamSelectMessage.Id);
+            Load();
+        }
+
+        private void DeleteGroup()
+        {
+            try
+            {
+                groupTaskRepository.DeleteGroup(Model.Id);
+            }
+            catch
+            {
+                messageBoxService.Show($"Deleting of '{Model?.Name}' failed!", "Deleting failed", MessageBoxButton.OK);
+                return;
+            }
+
+            mediator.Send(new GroupDeleteMessage { Id = Model.Id });
+
+            Model = null;
+        }
+
+        private void GroupDeleted(GroupDeleteMessage groupDeleteMessage)
+        {
             Load();
         }
 
