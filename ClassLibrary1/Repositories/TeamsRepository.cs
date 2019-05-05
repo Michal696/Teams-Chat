9346220b00862b665a14c09f4c0de101c52e3a5b
Martin Foltýn <admin@phpcode.eu>
2019-05-05 17:41:46 +0200

    fixed all tests

--- ClassLibrary1/Repositories/TeamsRepository.cs
+++ ClassLibrary1/Repositories/TeamsRepository.cs
@@ -8,6 +8,8 @@ using Teams.BL.Factories;
 using Teams.BL.Models;
 using Teams.BL.Mapper;
 using Teams.BL;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Teams.BL.Repositories
 {
@@ -19,21 +21,52 @@ namespace Teams.BL.Repositories
 
         public TeamMemberModel AddUserToTeam(TeamMemberModel TeamMember)
         {
-            using (var dbContext = dbContextFactory.CreateDbContext())
-            {
+           
                 var entity = mapper.TeamMemberModelToTeamMemberEntity(TeamMember);
+                if (entity.Id == null)
+                {
+                    entity.Id = Guid.NewGuid();
+                }
+                if (entity.Team.Id != null)
+                {
+                    dbContext.Team.Attach(entity.Team);
+                }
+                else
+                {
+                    entity.Team.Id = Guid.NewGuid();
+
+                }
+
+                if (entity.User.Id != null)
+                {
+                    dbContext.Users.Attach(entity.User);
+                }
+                else
+                {
+                    entity.User.Id = Guid.NewGuid();
+
+                }
+
                 dbContext.TeamMembers.Add(entity);
                 dbContext.SaveChanges();
                 return mapper.TeamMemberEntityToTeamMemberModel(entity);
-            }
+            
         }
 
         public  TeamModel Create(TeamModel model)
         {
             using (var dbContext = dbContextFactory.CreateDbContext())
             {
+                if (model.Id == null)
+                {
+                    model.Id = Guid.NewGuid();
+                }
+
                 var entity = mapper.TeamModelToTeamEntity(model);
+
+                
                 dbContext.Team.Add(entity);
+                
                 dbContext.SaveChanges();
                 return mapper.TeamEntityToTeamModel(entity);
             }
@@ -53,40 +86,76 @@ namespace Teams.BL.Repositories
             }
         }
 
+        
+        public void DeleteTeamMember(Guid Id)
+        {
+            using (var dbContext = dbContextFactory.CreateDbContext())
+            {
+                var team = new TeamMember
+                {
+                    Id = Id
+                };
+                dbContext.TeamMembers.Attach(team);
+                dbContext.TeamMembers.Remove(team);
+                dbContext.SaveChanges();
+            }
+        }
+
         public IEnumerable<TeamModel> GetAll()
         {
+            
             return dbContext.Team.Select(mapper.TeamEntityToTeamModel);
         }
 
         public TeamModel GetById(Guid Id)
         {
-            using (var dbContext = dbContextFactory.CreateDbContext())
-            {
-                var entity = dbContext.Team.FirstOrDefault(t => t.Id == Id);
-                return entity == null ? null : mapper.TeamEntityToTeamModel(entity);
-            }
+         
+            var entity = dbContext.Team.FirstOrDefault(t => t.Id == Id);
+            return entity == null ? null : mapper.TeamEntityToTeamModel(entity);
+           
         }
 
-        public IEnumerable<TeamModel> GetByUser(Guid Id)
+        /*  public IEnumerable<TeamModel> GetByUser(Guid Id)
+          {
+              using (var dbContext = dbContextFactory.CreateDbContext())
+              {
+
+                  var teamMemberEntity = dbContext.TeamMembers
+                      .Include(t => t.Team)
+                      .Select(mapper.TeamMemberEntityToTeamMemberModel)
+                      .Where(t => t.User.Id == Id);
+
+
+
+                  List<TeamModel> teamEntity = new List<TeamModel>();
+                  foreach (TeamMemberModel entity in teamMemberEntity.ToList())
+                  {
+                      Console.WriteLine("hey");
+                      Console.WriteLine(entity.Id);
+                      teamEntity.Concat(dbContext.Team
+                          .Select(mapper.TeamEntityToTeamModel)
+                          .Where(t => t.Id == entity.Team.Id));
+                  }
+
+
+
+                  return teamEntity == null ? null : teamEntity;
+              }
+          }
+          */
+        public IEnumerable<TeamMemberModel> GetMembershipsByUser(Guid Id)
         {
-            using (var dbContext = dbContextFactory.CreateDbContext())
-            {
-                var teamMemberEntity = dbContext.TeamMembers
-                    .Select(mapper.TeamMemberEntityToTeamMemberModel)
-                    .Where(t => t.User.Id == Id);
+           
+            var teamMemberEntities = dbContext.TeamMembers
+                .Include(t => t.Team)
+                .Select(mapper.TeamMemberEntityToTeamMemberModel)
+                .Where(t => t.User.Id == Id);
 
-                List<TeamModel> teamEntity = null;
-                foreach(TeamMemberModel entity in teamMemberEntity.ToList())
-                {
-                    teamEntity.Concat(dbContext.Team
-                        .Select(mapper.TeamEntityToTeamModel)
-                        .Where(t => t.Id == entity.Team.Id));
-                }
 
-                return teamEntity == null ? null : teamEntity;
-            }
+            return teamMemberEntities;
+            
         }
-
+       
         public void Update(TeamModel Team)
         {
             using (var dbContext = dbContextFactory.CreateDbContext())
