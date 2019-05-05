7e1d9c82387328a40927733b35fddedc1f257977
Jakub Liška <xliska16@vutbr.cz>
2019-05-05 23:51:27 +0200

    Popup edit team added

--- Teams/Views/EditTeamPopup.xaml.cs
+++ Teams/Views/EditTeamPopup.xaml.cs
@@ -10,25 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Teams.Views
 {
     /// <summary>
-    /// Interaction logic for MainMenuView.xaml
+    /// Interaction logic for EditTeamPopup.xaml
     /// </summary>
-    public partial class MainMenuView 
+    public partial class EditTeamPopup : Window
     {
-        public MainMenuView()
+        public EditTeamPopup()
         {
             InitializeComponent();
         }
-        private void btnEditName_Click(object sender, RoutedEventArgs e)
+        private void btnDialogOk_Click(object sender, RoutedEventArgs e)
         {
-            EditTeamPopup inputDialog = new EditTeamPopup();
-            if (inputDialog.ShowDialog() == true)
-                return;
+            this.DialogResult = true;
         }
+
     }
 }
