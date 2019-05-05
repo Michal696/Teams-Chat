1a2cadffb63edd74e0489de1fa2973eeafd8e886
Filip Pleško <xplesk02@vutbr.cz>
2019-05-05 15:52:15 +0200

    register

---
+++ Teams/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teams.Commands;
+using Teams.Services;
+using Teams.BL.Models;
+using Teams.BL.Repositories;
+using Teams.BL.Services;
+using Teams.BL.Messages;
+using Teams.BL.Extensions;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Windows;
+
+namespace Teams.ViewModels
+{
+    public class RegisterViewModel : ViewModelBase
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IMediator mediator;
+
+        public ICommand UserNewCommand { get; set; }
+
+        public RegisterViewModel(IUserRepository userRepository, IMediator mediator)
+        {
+            this.userRepository = userRepository;
+            this.mediator = mediator;
+
+            UserNewCommand = new RelayCommand(UserCreate);
+
+            mediator.Register<UserNewMessage>(UserCreated);
+        }
+
+        private void UserCreate()
+        {
+            User = new UserModel();
+            User.Id = Guid.NewGuid();
+
+            userRepository.Create(User);
+
+            mediator.Send(new UserNewMessage());
+        }
+
+        private void UserCreated(UserNewMessage obj)
+        {
+        }
+    }
+}
